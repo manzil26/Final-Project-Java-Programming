a57344fd351f3516159d6921d2af7007a42bcdcc
Final Project ASD <manzil260405@gmail.com>
2024-11-30 23:27:31 +0700

    for Project 3 rd semester of ASD subject ITS information System

---
+++ src/Main.java
@@ -0,0 +1,12 @@
+/**
+ * Main class of the Java program.
+ */
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println("Tic Tac Toe ");
+        TTT game = new TTT();
+        game.play();
+    }
+}
